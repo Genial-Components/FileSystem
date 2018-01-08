79f01aae6af5557e56ba1548da5c95aa12fdde15
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-08 10:39:51 -0500

    Create bootstrap.php

---
+++ vendor/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * @link      <https://github.com/Genial-Framework/Cryptography> for the canonical source repository.
+ * @copyright Copyright (c) 2017-2018 Genial Framework. <https://github.com/Genial-Components>
+ * @license   <https://github.com/Genial-Framework/Cryptography/blob/master/LICENSE> New BSD License.
+ */
+ 
+require 'vendor/autoload.php';
