3a7cd7dc75770130b09db5f878421e87ea84678d
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-08 13:25:26 -0500

    Create ExceptionInterface.php

---
+++ src/Exception/ExceptionInterface.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @link      <https://github.com/Genial-Framework/FileSystem> for the canonical source repository.
+ * @copyright Copyright (c) 2017-2018, Genial Framework. <https://github.com/Genial-Components>
+ * @license   <https://github.com/Genial-Framework/FileSystem/blob/master/LICENSE> New BSD License.
+ */
+ 
+namespace Genial\FileSystem\Exception;
+
+/**
+ * ExceptionInterface.
+ */
+interface ExceptionInterface
+{
+ 
+}
