563daa1e1d3a551c71d9e43bafee429112c4cfc7
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-08 14:09:06 -0500

    Update Handler.php

--- src/Handler.php
+++ src/Handler.php
@@ -1,1 +1,34 @@
+<?php
+/**
+ * @link      <https://github.com/Genial-Framework/FileSystem> for the canonical source repository.
+ * @copyright Copyright (c) 2017-2018, Genial Framework. <https://github.com/Genial-Components>
+ * @license   <https://github.com/Genial-Framework/FileSystem/blob/master/LICENSE> New BSD License.
+ */
+ 
+namespace Genial\FileSystem;
 
+use \Genial\FileSystem\Exception\{
+    RuntimeException
+};
+
+/**
+ * Handler.
+ */
+class Handler extends HandlerConfig implements HandlerInterface
+{
+  
+    function __construct(string $root = null)
+    {
+        $path = self::APP_ROOT . $root;
+        if (!\file_exists($path) && !\is_dir($path))
+        {
+            throw new RuntimeException(sprintf(
+                '`%s` The requested directory path does not exist. Passed: `%s`'
+                \__METHOD__,
+                htmlspecialchars($root, ENT_QUOTES, 'UTF-8')
+            ), 100);
+        }
+        
+    }
+ 
+}
