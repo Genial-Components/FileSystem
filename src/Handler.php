daaa37aec95c5cfe979dea85f99f12ffdc60e1c7
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-08 14:48:33 -0500

    Update Handler.php

--- src/Handler.php
+++ src/Handler.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * @link      <https://github.com/Genial-Framework/FileSystem> for the canonical source repository.
- * @copyright Copyright (c) 2017-2018, Genial Framework. <https://github.com/Genial-Components>
- * @license   <https://github.com/Genial-Framework/FileSystem/blob/master/LICENSE> New BSD License.
+ * @link      <https://github.com/Genial-Components/FileSystem> for the canonical source repository.
+ * @copyright Copyright (c) 2017-2018, Genial Framework. <https://github.com/Genial-Framework>
+ * @license   <https://github.com/Genial-Components/FileSystem/blob/master/LICENSE> New BSD License.
  */
  
 namespace Genial\FileSystem;
@@ -17,19 +17,34 @@ use \Genial\FileSystem\Exception\
  */
 class Handler extends HandlerConfig implements HandlerInterface
 {
+    
+    /**
+     * @var string $root The directory the handler should run on.
+     */
+    private $root;
   
+    /**
+     * __construct().
+     *
+     * Set the directory the handler should run on.
+     *
+     * @param string $root The directory the handler should run on.
+     *
+     * @throws RuntimeException If the requested directory path does not exist.
+     *
+     * @return void.
+     */
     function __construct(string $root): void
     {
-        $path = self::APP_ROOT . $root;
         if (!\file_exists($path) && !\is_dir($path))
         {
             throw new RuntimeException(\sprintf(
                 '`%s` The requested directory path does not exist. Passed: `%s`'
                 \__METHOD__,
                 \htmlspecialchars($root, \ENT_QUOTES, 'UTF-8')
-            ), 100);
+            ));
         }
-        
+        $this->root = $root;
     }
  
 }
