7bf9808492da271decb63791a2cdd5ec9f63a6c8
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-12 18:55:59 -0500

    Update ExceptionInterface.php

--- Exception/ExceptionInterface.php
+++ Exception/ExceptionInterface.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * @link      <https://github.com/Genial-Components/Cryptography> for the canonical source repository.
+ * @copyright Copyright (c) 2018 SyzerPHP <https://github.com/Genial-Framework>
+ * @license   <hztps://github.com/Genial-Components/Cryptography/blob/master/LICENSE> New BSD License.
+ */
 namespace Syzer\FileSystem\Exception;
 /**
  * ExceptionInterface.
