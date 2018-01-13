356d3a364ff2991e624aa3c8ad782d6dee612e6e
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-12 20:15:18 -0500

    Delete bootstrap.php

--- tests/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * @link      <https://github.com/Genial-Components/FileSystem> for the canonical source repository.
- * @copyright Copyright (c) 2017-2018, Genial Framework. <https://github.com/Genial-Framework>
- * @license   <https://github.com/Genial-Components/FileSystem/blob/master/LICENSE> New BSD License.
- */
- 
-require 'vendor/autoload.php';
