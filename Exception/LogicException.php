93cdb72d7e48299510b1dcb9e4d7c3af3bc6f486
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-12 20:07:21 -0500

    Update LogicException.php

--- Exception/LogicException.php
+++ Exception/LogicException.php
@@ -1,1 +1,13 @@
-
+<?php
+/**
+ * @link      <https://github.com/SyzerPHP/FileSystem> for the canonical source repository.
+ * @copyright Copyright (c) 2018, SyzerPHP <https://github.com/SyzerPHP>.
+ * @license   <hztps://github.com/SyzerPHP/FileSystem/blob/master/LICENSE> New BSD License.
+ */
+namespace Syzer\FileSystem\Exception;
+/**
+ * LogicException.
+ */
+class LogicException extends \LogicException implements ExceptionInterface
+{
+}
