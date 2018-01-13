e426dec24de84edca74395ee76f9aa19211e5d55
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-13 13:35:03 -0500

    Update Autoload.php

--- Autoload.php
+++ Autoload.php
@@ -1,2 +1,8 @@
 <?php
-FileSyste
+/**
+ * @link      <https://github.com/SyzerPHP/FileSystem> for the canonical source repository.
+ * @copyright Copyright (c) 2018, SyzerPHP <https://github.com/SyzerPHP>.
+ * @license   <hztps://github.com/SyzerPHP/FileSystem/blob/master/LICENSE> New BSD License.
+ */
+define('FILESYSTEM_COMPONENT', 'PRE-LOADED');
+define('FILESYSTEM_DIRS', ['Exception']);
