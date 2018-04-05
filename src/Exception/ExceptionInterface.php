cc89f99378a318064d860c2e55179a20fefe683b
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-04-04 21:08:19 -0400

    Update and rename Exception/ExceptionInterface.php to src/Exception/ExceptionInterface.php

---
+++ src/Exception/ExceptionInterface.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+/**
+ * FileSystem.
+ *
+ * @author  Nicholas English        <https://github.com/Nenglish7>.
+ * @author  FileSystem Contributors <https://github.com/Genial-Framework/FileSystem/graphs/contributors>.
+ *
+ * @link    <https://github.com/Genial-Framework/FileSystem> Github Repository.
+ * @license <https://github.com/Genial-Framework/FileSystem/master/LICENSE> MIT License.
+ */
+
+namespace Genial\FileSystem\Exception;
+
+use Throwable;
+
+/**
+ * ExceptionInterface.
+ */
+interface ExceptionInterface extends Throwable
+{
+}
