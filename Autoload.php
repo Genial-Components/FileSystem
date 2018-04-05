9cd519ff60afc768131caa8351365d9a6a232b3f
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-04-04 21:05:56 -0400

    Delete Autoload.php

--- Autoload.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * @link      <https://github.com/SyzerPHP/FileSystem> for the canonical source repository.
- * @copyright Copyright (c) 2018, SyzerPHP <https://github.com/SyzerPHP>.
- * @license   <hztps://github.com/SyzerPHP/FileSystem/blob/master/LICENSE> New BSD License.
- */
-define('FILESYSTEM_COMPONENT', 'PRE-LOADED');
-define('FILESYSTEM_NAMESPACE', 'Syzer\FileSystem');
-define('FILESYSTEM_DIRS', ['Exception']);
